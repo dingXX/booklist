6a69413ff928bcdfdc505e0bd57a47af210a22a1
cynthia <cynthia@futunn.com>
2018-11-28 20:38:04 +0800

    微信登录接口修改

--- src/api/api.js
+++ src/api/api.js
@@ -1,20 +1,10 @@
 import '../vendors/wxPromise.min.js';
-export function login() {
-    wx.pro.login().then(res => {
-        if (res.code) {
-            return wx.pro.request({
-                url:'https://booklist.futunn.com/api/login/index',
-                method:'POST',
-                data:{
-                    js_code:res.code
-                }
-            });
-        } else {
-            throw res;
+export function login(code) {
+    return wx.pro.request({
+        url:'https://booklist.futunn.com/api/login/index',
+        method:'POST',
+        data:{
+            js_code:code
         }
-    }).then(res =>{
-        console.log(res);
-    }).catch(err => {
-
     });
 }
--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 //app.js
-import {login} from './api/api';
+import {
+    login
+} from './api/api';
 App({
     onLaunch: function() {
-        login();
+        wx.pro.login().then(res => {
+            if (res.code) {
+                return login(res.code);
+            } else {
+                throw res;
+            }
+        }).then(res=>{
+            console.log(res.data);
+        }).catch(err=>{
+
+        });
     },
     globalData: {}
 })
